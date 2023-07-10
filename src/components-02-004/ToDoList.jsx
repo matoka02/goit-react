018c27c0d851145f8755a606c4544445a75d9805
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-10 11:51:02 +0300

    update

--- src/components-02-004/ToDoList.jsx
+++ src/components-02-004/ToDoList.jsx
@@ -3,35 +3,41 @@ import todo from 'assets/data-02-003.json';
 import ToDo from './ToDo';
 
 class ToDoList extends Component {
-    state={
-        todoList: todo
-    };
+  state = {
+    todoList: todo,
+  };
 
-    handleCheckCompleted = (id) => {
-        this.setState((prevState) => ({todoList: prevState.todoList.map(todo => todo.id === id? {...todo, completed: !todo.completed} : todo)}))
-    };
+  handleCheckCompleted = id => {
+    this.setState(prevState => ({
+      todoList: prevState.todoList.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      ),
+    }));
+  };
 
-    handleDelete = (id) => {
-        this.setState((prevState) => ({todoList: prevState.todoList.filter(todo => todo.id !== id)}))
-    };
+  handleDelete = id => {
+    this.setState(prevState => ({
+      todoList: prevState.todoList.filter(todo => todo.id !== id),
+    }));
+  };
 
-	render() {
-		return (
-			<>
-				<h1>My To-Do list</h1>
-				<ul className='list-group list-group-flush'>
-					{this.state.todoList.map((todo) => (
-						<ToDo
-							key={todo.id}
-							todo={todo}	
-                            handleCheckCompleted={this.handleCheckCompleted} 
-                            handleDelete={this.handleDelete}
-						/>
-					))}
-				</ul>
-			</>
-		)
-	}
+  render() {
+    return (
+      <>
+        <h1>My To-Do list</h1>
+        <ul className="list-group list-group-flush">
+          {this.state.todoList.map(todo => (
+            <ToDo
+              key={todo.id}
+              todo={todo}
+              handleCheckCompleted={this.handleCheckCompleted}
+              handleDelete={this.handleDelete}
+            />
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
 
 export default ToDoList;
