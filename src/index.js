934de7047b4ac3e383641d0fb51de753f771cb88
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-10 10:41:25 +0300

    update

--- src/index.js
+++ src/index.js
@@ -12,13 +12,14 @@ import './index.css';
 
 // 01. Introduction to React. Stylization
 // import './test/goit-test-r-01-001';
-import './test/goit-test-r-01-002';
+// import './test/goit-test-r-01-002';
 
 // 02. Events and States. Forms
 // import './test/goit-test-r-02-003';
 // import './test/goit-test-r-02-004';
 
 // 03. Lifecycle. HTTP Requests
+import './test/goit-test-r-03-005';
 
 // 04. React Hooks. Routing
 
