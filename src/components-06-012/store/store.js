4ff96481d0ef4cc415cb6ace7ee2dca5d79d8519
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-31 15:54:56 +0300

    Update store.js

--- src/components-06-012/store/store.js
+++ src/components-06-012/store/store.js
@@ -1,6 +1,15 @@
 // import { createStore } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
+
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 // import { reducer as rootReducer } from './store';
@@ -10,13 +19,21 @@ const persistConfig = {
   // key: 'root',
   key: 'todoS',
   storage,
-  blackList: ['counter']    // название ключа в reducer.js
+  blackList: ['counter'], // название ключа в reducer.js
 };
 
 const persistedReducer = persistReducer(persistConfig, reducer);
 
 // const store = configureStore({reducer: rootReducer});
 // const store = configureStore({reducer});
-export const store = configureStore({ reducer: persistedReducer });
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 export const persistor = persistStore(store);
