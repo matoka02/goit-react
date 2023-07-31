90fff169e9816c2f5148a768f46d19d4bafc2b42
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-31 23:01:40 +0300

    update lesson 20.04.2023

--- src/components-07-013/store/todo/todoReducer.js
+++ src/components-07-013/store/todo/todoReducer.js
@@ -1,28 +1,9 @@
-// import { initialState } from '../initialState';
 import { todoInitialState } from './initialState';
 import { createTodo } from './action';
-// import { CREATETODO } from './types';
 
 import { createReducer } from '@reduxjs/toolkit';
 
-
-// export const todoReducer = (state = initialState.todo, action) => {
-//   switch (action.type) {
-//     case CREATETODO:
-//       return {
-//         ...state,
-//         todo: [...state.todo, { ...action.payload }],
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
 export const todoReducer = createReducer(todoInitialState, {
-  // [createTodo]: (state, action) => ({
-  //   ...state,
-  //   todo: [...state.todo, { ...action.payload }],
-  // }),
   [createTodo]: (state, action) => {
     state.todo.push(action.payload)
   },
